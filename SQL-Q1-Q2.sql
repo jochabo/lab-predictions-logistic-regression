4c1d5b62d5639b64e646d2d5abd6161dee9fb12a
Josephine Bonvin <josephinecbonvin@gmail.com>
2022-12-09 09:35:20 +0100

    update2

--- SQL-Q1-Q2.sql
+++ SQL-Q1-Q2.sql
@@ -3,17 +3,20 @@ USE sakila;
 SELECT * FROM film;
 SELECT * FROM inventory;
 SELECT * FROM rental;
-
-SET sql_mode=(SELECT REPLACE(@@sql_mode,'ONLY_FULL_GROUP_BY',''));
+SELECT * FROM film_category;
 
 -- QUERY 1:
-SELECT f.film_id, f.title, r.rental_date, f.rental_rate, r.return_date, i.inventory_id, f.length, r.rental_id
+SELECT f.film_id,f.title, f.rental_duration, f.rental_rate, f.length, f.rating, fc.category_id
 FROM film f 
-JOIN inventory i
-ON f.film_id = i.film_id
-JOIN rental r
-ON i.inventory_id = r.inventory_id
-WHERE date_format((r.rental_date), "%Y")=2005;
+JOIN film_category fc USING (film_id)
+JOIN inventory i USING(film_id)
+JOIN rental r USING(inventory_id)
+WHERE date_format((r.rental_date), "%Y")=2005 
+GROUP BY f.film_id, f.title, f.rental_duration, f.rental_rate, f.length, f.rating, fc.category_id
+ORDER BY f.film_id ASC;
+
+ -- r.rental_date, r.return_date, 
+-- 
 
 -- QUERY 2:
 -- All the films that were rented in May 2005
@@ -26,36 +29,18 @@ WHERE date_format((r.rental_date), "%M") = "May" AND
 date_format((r.rental_date), "%Y") = 2005;
 
 
-SELECT film_id, 
-CASE WHEN film_id IN 
-(SELECT DISTINCT f.film_id 
+SELECT DISTINCT film_id, 
+CASE WHEN date_format((r.rental_date), "%M-%Y") = "May-2005" THEN True ELSE False END AS rented_in_may_2005
 FROM film f 
-JOIN inventory i
-ON f.film_id = i.film_id
-JOIN rental r
-ON i.inventory_id = r.inventory_id 
-WHERE date_format((r.rental_date), "%M") = "May" AND
-date_format((r.rental_date), "%Y") = 2005) THEN 1 ELSE 0 END AS rented_in_may_2005
-FROM film;
-
-SELECT f.title, 
-CASE 
-WHEN DATE(rental_date)
-BETWEEN '2005-05-01' AND '2005-05-31'
-THEN True
-ELSE False
-END AS rented_in_may_2005
-FROM rental r 
-JOIN inventory i USING (inventory_id)
-JOIN film f USING (film_id)
+JOIN inventory i USING(film_id)
+JOIN rental r USING(inventory_id)
 JOIN film_category fc USING (film_id)
-JOIN category c USING (category_id)
-GROUP BY f.title, day_type;
+WHERE date_format((r.rental_date), "%Y")=2005 ORDER BY film_id ASC;
 
+SELECT DISTINCT film_id, 
+CASE WHEN date_format((r.rental_date), "%M-%Y") = "May-2005" THEN True ELSE False END AS rented_in_may_2005
 FROM film f 
-JOIN inventory i
-ON f.film_id = i.film_id
-JOIN rental r
-ON i.inventory_id = r.inventory_id
-WHERE date_format((r.rental_date), "%Y")=2005;
+JOIN inventory i USING(film_id)
+JOIN rental r USING(inventory_id)
+GROUP BY film_id, rented_in_may_2005;
 
